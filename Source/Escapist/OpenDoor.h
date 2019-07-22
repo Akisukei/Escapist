a58976e5eade3459ff0bccc39a18a0a4c6dd1da1
Thanh (Justin) Nguyen <justin321261@hotmail.com>
2019-07-21 23:18:28 -0400

    added roof and timer for door closing

--- Source/Escapist/OpenDoor.h
+++ Source/Escapist/OpenDoor.h
@@ -22,17 +22,29 @@ protected:
 	virtual void BeginPlay() override;
 
 	void OpenDoor();
+	void CloseDoor();
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 private:
-	UPROPERTY(VisibleAnywhere)
+
+	/* UNREAL PROPERTIES */
+
+	//UPROPERTY(VisibleAnywhere)
+	UPROPERTY(EditAnywhere)
 	float OpenAngle = 50.f;
 		
 	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate;
 
+	UPROPERTY(EditAnywhere)
+	float DoorClosingDelay = 0.5f;
+
+	/* PRIVATE VARIABLES */
+	
+	AActor* Owner;
 	AActor* ActorThatOpens;
+	float DoorOpenedTime;	// time of pressure plate triggers door to open
 };
